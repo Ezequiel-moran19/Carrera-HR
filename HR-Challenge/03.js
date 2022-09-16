4e7a8bb343f0eb9240e9c6b48e84e1ba5c005784
Ezequiel Moran <moran.margot@hotmail.com>
2022-09-15 22:01:53 -0300

    Prueba ingreso a la carrera de Henry

---
+++ HR-Challenge/03.js
@@ -0,0 +1,33 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function buscarAmigo(amigos, nombre) {
+  // La funcion llamada 'buscarAmigo' recibe como argumento un array llamado 'amigos' que contiene
+  // en cada posición del arreglo un objeto que tiene como propiedades 'nombre' y 'edad'. También
+  // recibe un string llamado 'nombre'.
+  // Debe devolver el objeto cuya propiedad 'nombre' coincida con el string 'nombre' recibido por argumento.
+  // Ej:
+  //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
+  //  buscarAmigo(amigos, 'toni') debe devolver { nombre: 'toni', edad: 33 };
+
+  // Tu código aca:
+  let i = 0;
+  // Mientras queden elementos por comprobar en el array y el nombre del elemento actual no sea el nombre que buscamos, pasamos al elemento siguiente
+  while (i < amigos.length && amigos[i].nombre != nombre) {
+    i++;
+  }
+  
+  if (i < amigos.length) {
+    return amigos[i];
+  }
+  else {
+    return null;
+  }
+}
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = buscarAmigo
